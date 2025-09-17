55a18316e1956dcd32ab65f8ee28acf049b9c71c
toleibovitz <toleibovitz@gmail.com>
2025-09-16 23:49:20 -0500

    basic file server

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
 import (
+	"net/http"
 	
 )
+
+
+
+
+func main() {
+	handler := *http.NewServeMux()
+
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: &handler,
+	}
+	handler.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	
+
+	handler.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader((http.StatusOK))
+		
+		w.Write([]byte("OK"))
+	})
+
+	server.ListenAndServe()
+}
